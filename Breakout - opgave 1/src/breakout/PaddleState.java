fa009b9a421fc2d502fef22491ec2c0ad1deafce
Magarmanoj28 <91957611+Magarmanoj28@users.noreply.github.com>
2022-03-17 17:43:18 +0100

    Update PaddleState.java

--- Breakout - opgave 1/src/breakout/PaddleState.java
+++ Breakout - opgave 1/src/breakout/PaddleState.java
@@ -1,5 +1,18 @@
 package breakout;
 
 public class PaddleState {
-	// TODO: implement
+	private final Point center;
+	private final Vector size;
+	
+	public PaddleState(Point center, Vector size) {
+		this.center = center;
+		this.size = size;
+	}
+	public Point getTopLeft() {
+		return center.plus(size);
+	}
+	public Point getBottomRight() {
+		return center.minus(size);
+	}
 }
+//TODO: implement // Done
