3988207c49810e44ee47624033fc645fd02ddb7a
Magarmanoj28 <91957611+Magarmanoj28@users.noreply.github.com>
2022-03-17 17:42:03 +0100

    Update BreakoutState.java

--- Breakout - opgave 1/src/breakout/BreakoutState.java
+++ Breakout - opgave 1/src/breakout/BreakoutState.java
@@ -1,35 +1,49 @@
 package breakout;
 
+import java.awt.event.KeyEvent;
+
 // TODO: implement, document
 public class BreakoutState {
-
+	private Point bottomRight;
+	private BallState[] balls;
+	private PaddleState paddle;
+	private BlockState[] blocks;
 	
 	public BreakoutState(BallState[] balls, BlockState[] blocks, Point bottomRight, PaddleState paddle) {
+		this.balls = balls;
+		this.blocks = blocks;
+		this.bottomRight = bottomRight;
+		this.paddle = paddle;
 	}
+
 	
 	public BallState[] getBalls() {
-		return null;
+		return balls;
 	}
 
 	public BlockState[] getBlocks() {
-		return null;
+		return blocks;
 	}
 
 	public PaddleState getPaddle() {
-		return null;
+		return paddle;
 	}
 
 	public Point getBottomRight() {
-		return null;
+		return bottomRight;
 	}
 
 	public void tick(int paddleDir) {
 	}
 
 	public void movePaddleRight() {
+
+		
 	}
 
 	public void movePaddleLeft() {
+		
+		
 	}
 	
 	public boolean isWon() {
