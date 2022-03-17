eb5c70a3e48f2aeee59847c46b5f137757664426
Magarmanoj28 <91957611+Magarmanoj28@users.noreply.github.com>
2022-03-17 17:41:08 +0100

    Update BlockState.java

--- Breakout - opgave 1/src/breakout/BlockState.java
+++ Breakout - opgave 1/src/breakout/BlockState.java
@@ -1,5 +1,20 @@
 package breakout;
 
 public class BlockState {
-	// TODO: implement
+	private final Point topLeft;
+	private final Point bottomRight;
+	
+	public BlockState(Point topLeft, Point bottomRight){
+		this.topLeft = topLeft;
+		this.bottomRight = bottomRight;
+	}
+	
+	public Point getTopLeft() {
+		return topLeft;
+	}
+	public Point getBottomRight() {
+		return bottomRight;
+	}
+
 }
+//TODO: implement // Done
