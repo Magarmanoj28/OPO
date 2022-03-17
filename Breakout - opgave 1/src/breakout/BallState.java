2484c20eaac4bed00e239c165713c9e1b3f180b8
Magarmanoj28 <91957611+Magarmanoj28@users.noreply.github.com>
2022-03-17 17:40:45 +0100

    Update BallState.java

--- Breakout - opgave 1/src/breakout/BallState.java
+++ Breakout - opgave 1/src/breakout/BallState.java
@@ -1,13 +1,34 @@
 package breakout;
 
 public class BallState {
-	// TODO: implement
 	
+	private final Point center;
+	private final int diameter;
+	private final Vector velocity;
+	
+	
+	
+	
+	public BallState(Point center, int diameter, Vector velocity) {
+		this.center = center;
+		this.diameter = diameter;
+		this.velocity = velocity;
+	}
+
 	public Point getCenter() {
-		return null;
+		return center;
 	}
 	
 	public Vector getVelocity() {
-		return null;
+		return velocity;
 	}
+	public Point getTopleft() {
+		return center.plus(new Vector(diameter/2, diameter/2));
+	}
+	public Point getBottomRight() {
+		return center.minus(new Vector(diameter/2, diameter/2));
+	}
+	
 }
+
+//TODO: implement // Done
