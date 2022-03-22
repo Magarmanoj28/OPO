35051afbca434a756dae4f07884e8b740824212f
Magarmanoj28 <91957611+Magarmanoj28@users.noreply.github.com>
2022-03-22 21:07:05 +0100

    Update BallState.java

--- Breakout - opgave 1/src/breakout/BallState.java
+++ Breakout - opgave 1/src/breakout/BallState.java
@@ -1,20 +1,35 @@
 package breakout;
-
+/**
+ * @immutable
+ * @invar | getTopLeft < getBottomRight
+ */
 public class BallState {
-	
+	/**
+	 * @invar | topLeft < bottomRight
+	 * @post getCenter = center 
+	 * @post getVelocity = velocity
+	 * @post getDiameter = diameter
+	 */	
 	private final Point center;
 	private final int diameter;
 	private final Vector velocity;
 	
-	
-	
-	
-	public BallState(Point center, int diameter, Vector velocity) {
+	public BallState (Point center, int diameter, Vector velocity) {
 		this.center = center;
 		this.diameter = diameter;
 		this.velocity = velocity;
 	}
-
+	/**
+	 * @post getDiameter = b.getDiameter()
+	 * @post getVelocity = b.getVelocity()
+	 * @post getCenter = b.getCenter().plus(velocity)
+	 */
+	public BallState (BallState b) {
+		this.diameter = b.getDiameter();
+		this.velocity = b.getVelocity();
+		this.center = b.getCenter().plus(velocity);
+	}
+	
 	public Point getCenter() {
 		return center;
 	}
@@ -22,13 +37,18 @@ public class BallState {
 	public Vector getVelocity() {
 		return velocity;
 	}
-	public Point getTopleft() {
-		return center.minus(new Vector(diameter/2, diameter/2));
+	
+	public int getDiameter() {
+		return diameter;
 	}
+	
+	public Point getTopLeft() {
+		return center.minus(new Vector(diameter / 2, diameter / 2));
+	}
+	
 	public Point getBottomRight() {
-		return center.plus(new Vector(diameter/2, diameter/2));
+		return center.plus(new Vector(diameter / 2, diameter / 2));
 	}
 	
 }
-
 //TODO: implement // Done
